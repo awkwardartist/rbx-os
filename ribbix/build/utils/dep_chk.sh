5403b173ac8053d879f6bfd677ba9143696f7393
awkwardartist <73862490+awkwardartist@users.noreply.github.com>
2023-01-29 11:52:52 -0500

    Refactor

--- ribbix/build/utils/dep_chk.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# checks for all source dependencies,
-# is location-independent and
-# automatically resolves location
-
-RIBBIX_BASE="$(realpath $0)"
-REPO_BASE="$(realpath $RIBBIX_BASE/..)"
-
